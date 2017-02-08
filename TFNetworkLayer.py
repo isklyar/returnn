1d1decfb80143b574167ee80e2e30abfdd46b326
Anton Gerasimov <anton.gerasimov@rwth-aachen.de>
2017-02-08 16:16:02 +0100

    test1

--- TFNetworkLayer.py
+++ TFNetworkLayer.py
@@ -1,5 +1,7 @@
 
 import tensorflow as tf
+import sugartensor as stf
+
 from TFUtil import Data, OutputWithActivation
 
 
@@ -539,6 +541,70 @@ class FsaLayer(LayerBase):
     """
     super(FsaLayer, self).__init__(**kwargs)
     # TODO...
+
+    class DilatedLayer(LayerBase):
+  layer_class = "dilation"
+
+  # Computes a 1-D convolution given 3-D input and filter tensors
+  def conv2d(x, W, b, strides=1):
+    batch_size = 4  # batch size
+    num_blocks = 3  # dilated blocks
+    num_dim = 128  # latent dimension
+    # Conv2D wrapper, with bias and relu activation
+    #data = VCTK(batch_size=batch_size)
+
+    # vocabulary size
+    voca_size = data.voca_size
+
+    # mfcc feature of audio
+    #x = data.mfcc
+
+    # sequence length except zero-padding
+    seq_len = stf.not_equal(x.sg_sum(dims=2), 0.).sg_int().sg_sum(dims=1)
+
+    # target sentence label
+    #y = data.label
+
+  # residual block
+  def res_block(tensor, size, rate, dim=num_dim):
+
+    # filter convolution
+    conv_filter = tensor.sg_aconv1d(size=size, rate=rate, act='tanh', bn=True)
+
+    # gate convolution
+    conv_gate = tensor.sg_aconv1d(size=size, rate=rate, act='sigmoid', bn=True)
+
+    # output by gate multiplying
+    out = conv_filter * conv_gate
+
+    # final output
+    out = out.sg_conv1d(size=1, dim=dim, act='tanh', bn=True)
+
+    # residual and skip output
+    return out + tensor, out
+
+  # expand dimension
+  z = x.sg_conv1d(size=1, dim=num_dim, act='tanh', bn=True)
+
+  # dilated conv block loop
+  skip = 0  # skip connections
+  for i in range(num_blocks):
+    for r in [1, 2, 4, 8, 16]:
+      z, s = res_block(z, size=7, rate=r)
+      skip += s
+
+  # final logit layers
+  logit = (skip
+           .sg_conv1d(size=1, act='tanh', bn=True)
+           .sg_conv1d(size=1, dim=voca_size))
+
+  # CTC loss
+  loss = logit.sg_ctc(target=y, seq_len=seq_len)
+
+  # train
+  stf.sg_train(log_interval=30, lr=0.0001, loss=loss,
+              ep_size=data.num_batch, max_ep=20, early_stop=False)
+
 
 
 class CombineLayer(LayerBase):
